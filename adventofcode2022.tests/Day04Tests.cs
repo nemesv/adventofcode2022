e0886b7dc74102709549ea66ead03965ca3b8b66
Viktor Nemes <nemesv@gmail.com>
2022-12-04 06:54:58 +0100

    Solve Day04

--- adventofcode2022.tests/Day04Tests.cs
+++ adventofcode2022.tests/Day04Tests.cs
@@ -3,18 +3,18 @@
 public class Day04Tests : DayTest<Day04>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8", "2")]
     public void Part1(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part1();
+        var result = Sut(input).Part1();
         result.Should().Be(output);
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8", "4")]
     public void Part2(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part2();
+        var result = Sut(input).Part2();
         result.Should().Be(output);
     }
 }
--- adventofcode2022/Day04.cs
+++ adventofcode2022/Day04.cs
@@ -8,11 +8,43 @@ public class Day04 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var result = 0;
+        foreach (var line in lines)
+        {
+            var parts = line.Split(',');
+            var first = parts[0].Split("-").Select(int.Parse).ToArray();
+            var second = parts[1].Split("-").Select(int.Parse).ToArray();
+           
+            if (first[0] <= second[0] && second[1] <= first[1] ||
+                second[0] <= first[0] && first[1] <= second[1]
+                )
+            {
+                result++;
+            }
+        }
+        return result.ToString();
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var result = 0;
+        foreach (var line in lines)
+        {
+            var parts = line.Split(',');
+            var first = parts[0].Split("-").Select(int.Parse).ToArray();
+            var second = parts[1].Split("-").Select(int.Parse).ToArray();
+
+            if (first[0] <= second[0] && second[0] <= first[1] ||
+                first[0] <= second[1] && second[1] <= first[1] ||
+                second[0] <= first[0] && first[0] <= second[1] ||
+                second[0] <= first[1] && first[1] <= second[1]
+                )
+            {
+                result++;
+            }
+        }
+        return result.ToString();
     }
 }
