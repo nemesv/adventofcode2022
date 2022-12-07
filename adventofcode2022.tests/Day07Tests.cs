27931dfa64e073019c79e5e9889c055b5bccfee6
Viktor Nemes <nemesv@gmail.com>
2022-12-07 07:00:34 +0100

    Solve Day07

--- adventofcode2022.tests/Day07Tests.cs
+++ adventofcode2022.tests/Day07Tests.cs
@@ -3,7 +3,7 @@
 public class Day07Tests : DayTest<Day07>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k", "95437")]
     public void Part1(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part1();
@@ -11,7 +11,7 @@ public class Day07Tests : DayTest<Day07>
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k", "24933642")]
     public void Part2(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part2();
--- adventofcode2022/Day07.cs
+++ adventofcode2022/Day07.cs
@@ -8,11 +8,90 @@ public class Day07 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        return GetDirectories().Where(d => d.Value.GetTotalSize() <= 100000).Sum(d => d.Value.GetTotalSize()).ToString();
+    }
+
+    public Dictionary<string, Node> GetDirectories()
+    {
+        var root = new Node() { Name = "/" };
+        var current = root;
+        var directories = new Dictionary<string, Node>();
+        directories["/"] = root;
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            if (line.StartsWith("$ cd"))
+            {
+                var dir = line.Split(" ")[2];
+                if (dir == "/")
+                {
+                    current = root;
+                }
+                else if (dir == "..")
+                {
+                    current = current.Parent;
+                }
+                else
+                {
+                    var next = current.Children.SingleOrDefault(n => n.Name == dir);
+                    if (next == null)
+                    {
+                        next = new Node() { Name = dir, Path = $"{current.Path}/{dir}", Parent = current };
+                        directories[next.Path] = next;
+                    }
+                    current.Children.Add(next);
+                    current = next;
+                }
+            }
+            else if (line.StartsWith("$ ls"))
+            {
+                continue;
+            }
+            else
+            {
+                var parts = line.Split(" ");
+                if (parts[0] == "dir")
+                {
+                    continue;
+                }
+                else
+                {
+                    current.Children.Add(new Node() { Name = parts[1], Size = int.Parse(parts[0]) });
+                }
+            }
+        }
+        return directories;
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var directories = GetDirectories();
+        var totalSpace = 70000000;
+        var freeSpaceNeeded = 30000000;
+        var currentSpace = directories["/"].GetTotalSize();
+        var currentFree = totalSpace - currentSpace;
+        var deleteSize = freeSpaceNeeded - currentFree;
+        var directoryToDelete = directories.Where(d => d.Value.GetTotalSize() >= deleteSize).OrderBy(d => d.Value.GetTotalSize()).First();
+
+        return directoryToDelete.Value.GetTotalSize().ToString();
+    }
+}
+
+public class Node
+{
+    public Node Parent { get; set; }
+
+    public string Name { get; set; }
+
+    public string Path { get; set; }
+
+    public List<Node> Children { get; } = new List<Node>();
+
+    public int? Size { get; set; }
+
+    public int GetTotalSize()
+    {
+        var result = Size ?? 0;
+        result += Children.Sum(c => c.GetTotalSize());
+        return result;
     }
 }
