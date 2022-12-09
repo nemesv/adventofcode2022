abfee85348270cd84e8acc63c75c5f7e6a42f3d0
Viktor Nemes <nemesv@gmail.com>
2022-12-09 17:43:33 +0100

    Solve Day09

--- adventofcode2022.tests/Day09Tests.cs
+++ adventofcode2022.tests/Day09Tests.cs
@@ -3,7 +3,7 @@
 public class Day09Tests : DayTest<Day09>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("R 4\r\nU 4\r\nL 3\r\nD 1\r\nR 4\r\nD 1\r\nL 5\r\nR 2", "13")]
     public void Part1(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part1();
@@ -11,7 +11,7 @@ public class Day09Tests : DayTest<Day09>
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20", "36")]
     public void Part2(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part2();
--- adventofcode2022/Day09.cs
+++ adventofcode2022/Day09.cs
@@ -8,11 +8,67 @@ public class Day09 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        return NumberOfVisited(2).ToString();
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        return NumberOfVisited(10).ToString();
+    }
+
+    public int NumberOfVisited(int ropeLength)
+    {
+        var visited = new HashSet<(int row, int column)>();
+
+        var rope = new List<(int row, int column)>();
+        for (int i = 0; i < ropeLength; i++)
+        {
+            rope.Add((row: 0, column: 0));
+        }
+        visited.Add((row: 0, column: 0));
+        foreach (var instructions in input.Split(Environment.NewLine))
+        {
+            var head = rope[0];
+            var parts = instructions.Split(" ");
+            var direction = parts[0];
+            var steps = int.Parse(parts[1]);
+            for (int i = 0; i < steps; i++)
+            {
+                switch (direction)
+                {
+                    case "R":
+                        head = (head.row, head.column + 1);
+                        break;
+                    case "L":
+                        head = (head.row, head.column - 1);
+                        break;
+                    case "U":
+                        head = (head.row + 1, head.column);
+                        break;
+                    case "D":
+                        head = (head.row - 1, head.column);
+                        break;
+                }
+                rope[0] = head;
+                for (int ropePart = 1; ropePart < rope.Count; ropePart++)
+                {
+                    var prev = rope[ropePart - 1];
+                    var current = rope[ropePart];
+
+                    var rowDiff = prev.row - current.row;
+                    var columnDiff = prev.column - current.column;
+                    if (Math.Abs(columnDiff) > 1 || Math.Abs(rowDiff) > 1)
+                    {
+                        current = (current.row + Math.Sign(rowDiff) * 1, current.column + Math.Sign(columnDiff) * 1);
+                        rope[ropePart] = current;
+
+                        if (ropePart == rope.Count - 1)
+                            visited.Add(rope[ropePart]);
+                    }
+                }
+            }
+        }
+        return visited.Count;
     }
 }
+
