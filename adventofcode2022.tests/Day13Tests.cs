557a8382ec2d57097b344e52a07e51f27107e052
Viktor Nemes <nemesv@gmail.com>
2022-12-13 08:09:50 +0100

    Solve Day13

--- adventofcode2022.tests/Day13Tests.cs
+++ adventofcode2022.tests/Day13Tests.cs
@@ -3,18 +3,18 @@
 public class Day13Tests : DayTest<Day13>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("[1,1,3,1,1]\r\n[1,1,5,1,1]\r\n\r\n[[1],[2,3,4]]\r\n[[1],4]\r\n\r\n[9]\r\n[[8,7,6]]\r\n\r\n[[4,4],4,4]\r\n[[4,4],4,4,4]\r\n\r\n[7,7,7,7]\r\n[7,7,7]\r\n\r\n[]\r\n[3]\r\n\r\n[[[]]]\r\n[[]]\r\n\r\n[1,[2,[3,[4,[5,6,7]]]],8,9]\r\n[1,[2,[3,[4,[5,6,0]]]],8,9]", "13")]
     public void Part1(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part1();
+        var result = Sut(input).Part1();
         result.Should().Be(output);
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("[1,1,3,1,1]\r\n[1,1,5,1,1]\r\n\r\n[[1],[2,3,4]]\r\n[[1],4]\r\n\r\n[9]\r\n[[8,7,6]]\r\n\r\n[[4,4],4,4]\r\n[[4,4],4,4,4]\r\n\r\n[7,7,7,7]\r\n[7,7,7]\r\n\r\n[]\r\n[3]\r\n\r\n[[[]]]\r\n[[]]\r\n\r\n[1,[2,[3,[4,[5,6,7]]]],8,9]\r\n[1,[2,[3,[4,[5,6,0]]]],8,9]", "140")]
     public void Part2(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part2();
+        var result = Sut(input).Part2();
         result.Should().Be(output);
     }
 }
--- adventofcode2022/Day13.cs
+++ adventofcode2022/Day13.cs
@@ -1,4 +1,7 @@
-﻿namespace adventofcode2022;
+﻿using System.ComponentModel;
+using System.Security.AccessControl;
+
+namespace adventofcode2022;
 
 public class Day13 : Day
 {
@@ -8,11 +11,151 @@ public class Day13 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var result = 0;
+        var index = 1;
+        for (int i = 0; i < lines.Length; i += 3)
+        {
+            var leftString = lines[i];
+            var rightString = lines[i + 1];
+            var left = Parse(leftString);
+            var right = Parse(rightString);
+
+            var isInOrder = IsInOrder(left, right);
+
+            if (isInOrder == true)
+            {
+                result += index;
+            }
+
+            index++;
+        }
+
+        return result.ToString();
+    }
+
+    public bool? IsInOrder(Signal left, Signal right)
+    {
+        if (left.Value.HasValue && right.Value.HasValue)
+        {
+            if (left.Value == right.Value)
+            {
+                return null;
+            }
+            return left.Value < right.Value;
+        }
+        else if (left.Value == null && right.Value == null)
+        {
+            for (int i = 0; i < left.Inner.Count; i++)
+            {
+                if (i < right.Inner.Count)
+                {
+                    var result = IsInOrder(left.Inner[i], right.Inner[i]);
+                    if (result.HasValue)
+                        return result.Value;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return left.Inner.Count < right.Inner.Count ? true : null;
+        }
+        else if (left.Value == null)
+        {
+            var wrapper = new Signal();
+            wrapper.Inner.Add(new Signal() { Value = right.Value });
+            return IsInOrder(left, wrapper);
+        }
+        else //right.Value = null
+        {
+            var wrapper = new Signal();
+            wrapper.Inner.Add(new Signal() { Value = left.Value });
+            return IsInOrder(wrapper, right);
+        }
+    }
+
+    public Signal Parse(string inputString)
+    {
+        Signal root = null;
+        Signal current = null;
+        for (int j = 0; j < inputString.Length - 1; j++)
+        {
+            switch (inputString[j])
+            {
+                case '[':
+                    var newSignal = new Signal();
+                    if (root == null)
+                    {
+                        root = newSignal;
+                    }
+                    else
+                    {
+                        newSignal.Parent = current;
+                        current.Inner.Add(newSignal);
+                    }
+                    current = newSignal;
+                    break;
+                case ']':
+                    current = current.Parent;
+                    break;
+                case ',':
+                    break;
+                default:
+                    var n = inputString[j].ToString();
+                    if (char.IsDigit(inputString[j + 1]))
+                        n += inputString[j + 1];
+                    current.Inner.Add(new Signal() { Value = int.Parse(n), Parent = current });
+                    break;
+            }
+        }
+        return root;
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var result = 0;
+        var index = 1;
+        var signals = new List<Signal>();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (!string.IsNullOrEmpty(lines[i]))
+                signals.Add(Parse(lines[i]));
+        }
+        var two = new Signal()
+        {
+            Inner = { new Signal() { Inner = { new Signal() { Value = 2 } } } }
+        };
+        var six = new Signal()
+        {
+            Inner = { new Signal() { Inner = { new Signal() { Value = 6 } } } }
+        };
+        signals.Add(two);
+        signals.Add(six);
+
+        signals.Sort((x, y) =>
+        {
+            switch (IsInOrder(y, x))
+            {
+                case true:
+                    return 1;
+                case false:
+                    return -1;
+                case null:
+                    return 0;
+            }
+        });
+
+        return ((signals.IndexOf(two) + 1) * (signals.IndexOf(six) + 1)).ToString();
+    }
+
+    public class Signal
+    {
+        public int? Value { get; set; }
+
+        public List<Signal> Inner { get; } = new List<Signal>();
+
+        public Signal Parent { get; set; }
     }
 }
