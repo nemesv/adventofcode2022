7b8c6b5ddb3679cc7445aa2fe7f9651570750738
Viktor Nemes <nemesv@gmail.com>
2022-12-14 07:25:50 +0100

    Solve Day14

--- adventofcode2022.tests/Day14Tests.cs
+++ adventofcode2022.tests/Day14Tests.cs
@@ -3,18 +3,18 @@
 public class Day14Tests : DayTest<Day14>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("498,4 -> 498,6 -> 496,6\r\n503,4 -> 502,4 -> 502,9 -> 494,9", "24")]
     public void Part1(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part1();
+        var result = Sut(input).Part1();
         result.Should().Be(output);
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("498,4 -> 498,6 -> 496,6\r\n503,4 -> 502,4 -> 502,9 -> 494,9", "93")]
     public void Part2(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part2();
+        var result = Sut(input).Part2();
         result.Should().Be(output);
     }
 }
--- adventofcode2022/Day14.cs
+++ adventofcode2022/Day14.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode2022;
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace adventofcode2022;
 
 public class Day14 : Day
 {
@@ -8,11 +10,122 @@ public class Day14 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var rocks = new HashSet<(int, int)>();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(" -> ");
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                var from = parts[i].Split(",").Select(int.Parse).ToArray();
+                var to = parts[i + 1].Split(",").Select(int.Parse).ToArray();
+
+                for (int c = Math.Min(from[0], to[0]); c <= Math.Max(from[0], to[0]); c++)
+                {
+                    rocks.Add((c, from[1]));
+                }
+                for (int c = Math.Min(from[1], to[1]); c <= Math.Max(from[1], to[1]); c++)
+                {
+                    rocks.Add((from[0], c));
+                }
+            }
+        }
+        var maxDepth = rocks.Select(r => r.Item2).Max();
+        var sand = new HashSet<(int, int)>();
+
+        while (true)
+        {
+            var current = (500, 0);
+            var fallOff = false;
+            while (true)
+            {
+                var down = (current.Item1, current.Item2 + 1);
+                var left = (current.Item1 - 1, current.Item2 + 1);
+                var right = (current.Item1 + 1, current.Item2 + 1);
+                if (!rocks.Contains(down) && !sand.Contains(down))
+                {
+                    current = down;
+                }
+                else if (!rocks.Contains(left) && !sand.Contains(left))
+                {
+                    current = left;
+                }
+                else if (!rocks.Contains(right) && !sand.Contains(right))
+                {
+                    current = right;
+                }
+                else
+                {
+                    sand.Add(current);
+                    break;
+                }
+                if (current.Item2 > maxDepth)
+                {
+                    fallOff = true;
+                    break;
+                }
+            }
+            if (fallOff)
+                break;
+        }
+        return sand.Count.ToString();
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var rocks = new HashSet<(int, int)>();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(" -> ");
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                var from = parts[i].Split(",").Select(int.Parse).ToArray();
+                var to = parts[i + 1].Split(",").Select(int.Parse).ToArray();
+
+                for (int c = Math.Min(from[0], to[0]); c <= Math.Max(from[0], to[0]); c++)
+                {
+                    rocks.Add((c, from[1]));
+                }
+                for (int c = Math.Min(from[1], to[1]); c <= Math.Max(from[1], to[1]); c++)
+                {
+                    rocks.Add((from[0], c));
+                }
+            }
+        }
+        var maxDepth = rocks.Select(r => r.Item2).Max();
+        var sand = new HashSet<(int, int)>();
+
+        while (true)
+        {
+            var current = (500, 0);
+            var fallOff = false;
+            while (true)
+            {
+                var down = (current.Item1, current.Item2 + 1);
+                var left = (current.Item1 - 1, current.Item2 + 1);
+                var right = (current.Item1 + 1, current.Item2 + 1);
+                if (!rocks.Contains(down) && !sand.Contains(down) && down.Item2 < maxDepth + 2)
+                {
+                    current = down;
+                }
+                else if (!rocks.Contains(left) && !sand.Contains(left) && down.Item2 < maxDepth + 2)
+                {
+                    current = left;
+                }
+                else if (!rocks.Contains(right) && !sand.Contains(right) && down.Item2 < maxDepth + 2)
+                {
+                    current = right;
+                }
+                else
+                {
+                    sand.Add(current);
+                    break;
+                }
+            }
+            if (sand.Contains((500, 0)))
+                break;
+        }
+        return sand.Count.ToString();
     }
 }
