24be7c5c7580cd4761a206c0648d2a1765d199b2
Viktor Nemes <nemesv@gmail.com>
2022-12-15 07:49:46 +0100

    Solve Day15

--- adventofcode2022.tests/Day15Tests.cs
+++ adventofcode2022.tests/Day15Tests.cs
@@ -3,18 +3,18 @@
 public class Day15Tests : DayTest<Day15>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("Sensor at x=2, y=18: closest beacon is at x=-2, y=15\r\nSensor at x=9, y=16: closest beacon is at x=10, y=16\r\nSensor at x=13, y=2: closest beacon is at x=15, y=3\r\nSensor at x=12, y=14: closest beacon is at x=10, y=16\r\nSensor at x=10, y=20: closest beacon is at x=10, y=16\r\nSensor at x=14, y=17: closest beacon is at x=10, y=16\r\nSensor at x=8, y=7: closest beacon is at x=2, y=10\r\nSensor at x=2, y=0: closest beacon is at x=2, y=10\r\nSensor at x=0, y=11: closest beacon is at x=2, y=10\r\nSensor at x=20, y=14: closest beacon is at x=25, y=17\r\nSensor at x=17, y=20: closest beacon is at x=21, y=22\r\nSensor at x=16, y=7: closest beacon is at x=15, y=3\r\nSensor at x=14, y=3: closest beacon is at x=15, y=3\r\nSensor at x=20, y=1: closest beacon is at x=15, y=3", "26")]
     public void Part1(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part1();
+        var result = Sut(input).Part1(10);
         result.Should().Be(output);
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("Sensor at x=2, y=18: closest beacon is at x=-2, y=15\r\nSensor at x=9, y=16: closest beacon is at x=10, y=16\r\nSensor at x=13, y=2: closest beacon is at x=15, y=3\r\nSensor at x=12, y=14: closest beacon is at x=10, y=16\r\nSensor at x=10, y=20: closest beacon is at x=10, y=16\r\nSensor at x=14, y=17: closest beacon is at x=10, y=16\r\nSensor at x=8, y=7: closest beacon is at x=2, y=10\r\nSensor at x=2, y=0: closest beacon is at x=2, y=10\r\nSensor at x=0, y=11: closest beacon is at x=2, y=10\r\nSensor at x=20, y=14: closest beacon is at x=25, y=17\r\nSensor at x=17, y=20: closest beacon is at x=21, y=22\r\nSensor at x=16, y=7: closest beacon is at x=15, y=3\r\nSensor at x=14, y=3: closest beacon is at x=15, y=3\r\nSensor at x=20, y=1: closest beacon is at x=15, y=3", "56000011")]
     public void Part2(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part2();
+        var result = Sut(input).Part2(20);
         result.Should().Be(output);
     }
 }
--- adventofcode2022/Day15.cs
+++ adventofcode2022/Day15.cs
@@ -1,4 +1,8 @@
-﻿namespace adventofcode2022;
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace adventofcode2022;
 
 public class Day15 : Day
 {
@@ -8,11 +12,95 @@ public class Day15 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        return Part1(2000000);
+    }
+
+    //06:30
+    public string Part1(int targetY)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var spacesInRow = new HashSet<(int, int)>();
+        foreach (var line in lines)
+        {
+            var match = Regex.Match(line, @"Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)");
+            var parsed = match.Groups.Cast<Group>().Skip(1).Select(m => int.Parse(m.Value)).ToArray();
+
+            var distance = Math.Abs(parsed[0] - parsed[2]) + Math.Abs(parsed[1] - parsed[3]);
+
+            if (Math.Abs(parsed[1] - targetY) <= distance)
+            {
+                var verticalDistance = Math.Abs(parsed[1] - targetY);
+                for (int i = parsed[0] - distance + verticalDistance; i <= parsed[0] + distance - verticalDistance; i++)
+                {
+                    var r = Math.Abs(parsed[0] - i) + Math.Abs(parsed[1] - targetY);
+                    if (r <= distance)
+                    {
+                        if ((i, targetY) != (parsed[2], parsed[3]))
+                            spacesInRow.Add((i, targetY));
+                    }
+                }
+            }
+        }
+
+        return spacesInRow.Count.ToString();
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        return Part2(4000000);
+    }
+
+    public string Part2(int max)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var spacesInRow = new HashSet<(long, long)>();
+        var j = 0;
+        var sensors = new List<(int x, int y, int d)>();
+        foreach (var line in lines)
+        {
+            var match = Regex.Match(line, @"Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)");
+            var parsed = match.Groups.Cast<Group>().Skip(1).Select(m => int.Parse(m.Value)).ToArray();
+            var distance = Math.Abs(parsed[0] - parsed[2]) + Math.Abs(parsed[1] - parsed[3]);
+            sensors.Add((x: parsed[0], y: parsed[1], d: distance));
+        }
+        foreach (var sensor in sensors)
+        {
+            for (int y = sensor.y - (sensor.d + 1); y <= sensor.y + (sensor.d + 1); y++)
+            {
+                var d = Math.Abs(sensor.y - y);
+                var endLeft = sensor.x - (sensor.d + 1) + d;
+                var endRight = sensor.x + (sensor.d + 1) - d;
+                if (0 <= endLeft && endLeft <= max && 0 <= y && y <= max)
+                {
+                    var canAdd = true;
+                    foreach (var sensorInner in sensors)
+                    {
+                        if (Math.Abs(sensorInner.x - endLeft) + Math.Abs(sensorInner.y - y) <= sensorInner.d)
+                        {
+                            canAdd = false;
+                            break;
+                        }
+                    }
+                    if (canAdd)
+                        spacesInRow.Add((endLeft, y));
+                }
+                if (0 <= endRight && endRight <= max && 0 <= y && y <= max)
+                {
+                    var canAdd = true;
+                    foreach (var sensorInner in sensors)
+                    {
+                        if (Math.Abs(sensorInner.x - endRight) + Math.Abs(sensorInner.y - y) <= sensorInner.d)
+                        {
+                            canAdd = false;
+                            break;
+                        }
+                    }
+                    if (canAdd)
+                        spacesInRow.Add((endRight, y));
+                }
+            }
+        }
+        var result = spacesInRow.Single();
+        return (result.Item1 * 4000000 + result.Item2).ToString();
     }
 }
