2cbedb45800d93b0f48e5c5096914e93c502564c
Viktor Nemes <nemesv@gmail.com>
2022-12-18 09:27:14 +0100

    Solve Day18

--- adventofcode2022.tests/Day18Tests.cs
+++ adventofcode2022.tests/Day18Tests.cs
@@ -3,18 +3,18 @@
 public class Day18Tests : DayTest<Day18>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5", "64")]
     public void Part1(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part1();
+        var result = Sut(input).Part1();
         result.Should().Be(output);
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5", "58")]
     public void Part2(string input, string output)
     {
-        var result = Sut(input.Replace(",", "\r\n")).Part2();
+        var result = Sut(input).Part2();
         result.Should().Be(output);
     }
 }
--- adventofcode2022/Day18.cs
+++ adventofcode2022/Day18.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode2022;
+﻿using System.Collections.Generic;
+
+namespace adventofcode2022;
 
 public class Day18 : Day
 {
@@ -8,11 +10,116 @@ public class Day18 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var cubes = input.Split(Environment.NewLine).Select(l => l.Split(",").Select(int.Parse).ToArray()).ToArray();
+
+        var result = 0;
+
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            var cube1 = cubes[i];
+            var openSides = 6;
+            for (int j = 0; j < cubes.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                var cube2 = cubes[j];
+                if (
+                    Math.Abs(cube1[0] - cube2[0]) +
+                    Math.Abs(cube1[1] - cube2[1]) +
+                    Math.Abs(cube1[2] - cube2[2])
+                    == 1
+                    )
+                    openSides--;
+            }
+            result += openSides;
+        }
+
+        return result.ToString();
+    }
+
+    private List<(int, int, int)> GetNeighbours((int, int, int) current)
+    {
+        var neighbours = new List<(int, int, int)>()
+                {
+                    (current.Item1, current.Item2, current.Item3 + 1),
+                    (current.Item1, current.Item2, current.Item3 - 1),
+                    (current.Item1, current.Item2 + 1, current.Item3),
+                    (current.Item1, current.Item2 - 1, current.Item3),
+                    (current.Item1 + 1, current.Item2, current.Item3),
+                    (current.Item1 - 1, current.Item2, current.Item3),
+                };
+        return neighbours;
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var cubes = input.Split(Environment.NewLine).Select(l => l.Split(",").Select(int.Parse).ToArray()).ToArray();
+
+        var air = new HashSet<(int, int, int)>();
+        var originalOpenSides = 0;
+        var cubesSet = new HashSet<(int, int, int)>(cubes.Select(c => (c[0], c[1], c[2])));
+
+        foreach (var cube in cubesSet)
+        {
+            foreach (var n in GetNeighbours(cube))
+            {
+                if (!cubesSet.Contains(n))
+                {
+                    originalOpenSides++;
+                    air.Add(n);
+                }
+            }
+        }
+        var internalAir = new List<(int, int, int)>();
+        var result = 0;
+        foreach (var airCube in air)
+        {
+            var pocket = new Queue<(int, int, int)>() { };
+            pocket.Enqueue(airCube);
+            var visited = new HashSet<(int, int, int)>();
+
+            while (true)
+            {
+                if (pocket.Count == 0)
+                {
+                    foreach (var item in visited)
+                    {
+                        internalAir.Add(item);
+                    }
+                    break;
+                }
+                var current = pocket.Dequeue();
+                if (visited.Contains(current))
+                    continue;
+
+                visited.Add(current);
+                var neighbours = GetNeighbours(current);
+                foreach (var n in neighbours)
+                {
+                    if (cubesSet.Contains(n))
+                        continue;
+
+                    pocket.Enqueue(n);
+                }
+                if (visited.Count > air.Count)
+                    break;
+            }
+        }
+        foreach (var current in air)
+        {
+            if (internalAir.Contains(current))
+                continue;
+
+            var neighbours = GetNeighbours(current);
+            foreach (var n in neighbours)
+            {
+                if (cubesSet.Contains(n))
+                {
+                    result++;
+                }
+            }
+        }
+        return result.ToString();
     }
 }
