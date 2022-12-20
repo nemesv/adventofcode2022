daa62908c4860c91ab3e0b0d8592c7ad346f1593
Viktor Nemes <nemesv@gmail.com>
2022-12-20 19:16:02 +0100

    Solve Day20

--- adventofcode2022.tests/Day20Tests.cs
+++ adventofcode2022.tests/Day20Tests.cs
@@ -3,7 +3,7 @@
 public class Day20Tests : DayTest<Day20>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("1\r\n2\r\n-3\r\n3\r\n-2\r\n0\r\n4", "3")]
     public void Part1(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part1();
@@ -11,7 +11,7 @@ public class Day20Tests : DayTest<Day20>
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("1\r\n2\r\n-3\r\n3\r\n-2\r\n0\r\n4", "1623178306")]
     public void Part2(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part2();
--- adventofcode2022/Day20.cs
+++ adventofcode2022/Day20.cs
@@ -1,4 +1,7 @@
-﻿namespace adventofcode2022;
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace adventofcode2022;
 
 public class Day20 : Day
 {
@@ -8,11 +11,128 @@ public class Day20 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        return Solve(1, 1);
     }
-
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        return Solve(10, 811589153L);
+    }
+
+    private string Solve(int turns, long multiplier)
+    {
+        var numbers = input.Split(Environment.NewLine).Select(int.Parse).ToList();
+        var current = new Node() { Value = numbers[0] * multiplier };
+        var nodes = new List<Node>() { current };
+        var start = current;
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            var next = new Node() { Value = numbers[i] * multiplier };
+            current.Next = next;
+            next.Prev = current;
+            current = next;
+            nodes.Add(next);
+        }
+        var end = current;
+        end.Next = start;
+        start.Prev = end;
+
+        for (int i = 0; i < turns; i++)
+        {
+
+            foreach (var node in nodes)
+            {
+                var currentNode = node;
+                if (currentNode.Value > 0)
+                {
+                    var move = currentNode.Value % (nodes.Count - 1);
+                    while (move > 0)
+                    {
+                        currentNode = node;
+
+                        var toSwap = currentNode.Next;
+                        var next = toSwap.Next;
+                        var prev = currentNode.Prev;
+
+                        if (currentNode == start)
+                            start = toSwap;
+
+                        prev.Next = toSwap;
+
+                        toSwap.Prev = prev;
+                        toSwap.Next = currentNode;
+
+                        currentNode.Next = next;
+                        currentNode.Prev = toSwap;
+
+                        next.Prev = currentNode;
+
+                        move--;
+                    }
+                }
+                else
+                {
+                    var move = BigInteger.Abs(currentNode.Value) % (nodes.Count - 1);
+                    while (move > 0)
+                    {
+                        currentNode = node;
+
+                        var toSwap = currentNode;
+                        currentNode = toSwap.Prev;
+
+                        var next = toSwap.Next;
+                        var prev = currentNode.Prev;
+
+                        if (currentNode == start)
+                            start = toSwap;
+
+                        prev.Next = toSwap;
+
+                        toSwap.Prev = prev;
+                        toSwap.Next = currentNode;
+
+                        currentNode.Next = next;
+                        currentNode.Prev = toSwap;
+
+                        next.Prev = currentNode;
+
+                        if (toSwap == start)
+                            start = currentNode;
+
+                        move--;
+                    }
+                }
+            }
+        }
+        var zero = nodes.Where(n => n.Value == 0).First();
+        var resultNodes = new List<Node>();
+        var s = zero;
+        while (true)
+        {
+            resultNodes.Add(s);
+            s = s.Next;
+            if (s == zero)
+                break;
+        }
+        var result = new BigInteger(0);
+        foreach (var index in new[] { 1000, 2000, 3000 })
+        {
+            result += resultNodes[index % resultNodes.Count].Value;
+        }
+
+        return result.ToString();
+    }
+
+    public class Node
+    {
+        public BigInteger Value { get; set; }
+
+        public Node Prev { get; set; }
+
+        public Node Next { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Prev.Value} -> {Value} -> {Next.Value}";
+        }
     }
 }
