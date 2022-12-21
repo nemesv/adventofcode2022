a272132816dcadb2ec4a54baa054d6de3b76a738
Viktor Nemes <nemesv@gmail.com>
2022-12-21 09:03:07 +0100

    Solve Day21

--- adventofcode2022.tests/Day21Tests.cs
+++ adventofcode2022.tests/Day21Tests.cs
@@ -3,7 +3,7 @@
 public class Day21Tests : DayTest<Day21>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("root: pppw + sjmn\r\ndbpl: 5\r\ncczh: sllz + lgvd\r\nzczc: 2\r\nptdq: humn - dvpt\r\ndvpt: 3\r\nlfqf: 4\r\nhumn: 5\r\nljgn: 2\r\nsjmn: drzm * dbpl\r\nsllz: 4\r\npppw: cczh / lfqf\r\nlgvd: ljgn * ptdq\r\ndrzm: hmdt - zczc\r\nhmdt: 32", "152")]
     public void Part1(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part1();
@@ -11,7 +11,7 @@ public class Day21Tests : DayTest<Day21>
     }
 
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("root: pppw + sjmn\r\ndbpl: 5\r\ncczh: sllz + lgvd\r\nzczc: 2\r\nptdq: humn - dvpt\r\ndvpt: 3\r\nlfqf: 4\r\nhumn: 5\r\nljgn: 2\r\nsjmn: drzm * dbpl\r\nsllz: 4\r\npppw: cczh / lfqf\r\nlgvd: ljgn * ptdq\r\ndrzm: hmdt - zczc\r\nhmdt: 32", "301")]
     public void Part2(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part2();
--- adventofcode2022/Day21.cs
+++ adventofcode2022/Day21.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode2022;
+﻿using System.Text.RegularExpressions;
+
+namespace adventofcode2022;
 
 public class Day21 : Day
 {
@@ -8,11 +10,164 @@ public class Day21 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var values = new Dictionary<string, long>();
+        var operators = new Dictionary<string, (string arg1, string arg2, string op)>();
+        foreach (var line in lines)
+        {
+            var match = Regex.Match(line, @"(.*): (.*)");
+            var name = match.Groups[1].Value;
+            var args = match.Groups[2].Value.Split(" ");
+            if (args.Length == 1)
+            {
+                values[name] = long.Parse(args[0]);
+            }
+            else
+            {
+                operators[name] = (args[0], args[2], args[1]);
+            }
+        }
+        while (operators.Count > 0)
+        {
+            var canEvaluate = operators.Where(op => values.ContainsKey(op.Value.arg1) && values.ContainsKey(op.Value.arg2));
+            foreach (var op in canEvaluate)
+            {
+                var arg1 = values[op.Value.arg1];
+                var arg2 = values[op.Value.arg2];
+                var result = 0L;
+                switch (op.Value.op)
+                {
+                    case "+":
+                        result = arg1 + arg2;
+                        break;
+                    case "-":
+                        result = arg1 - arg2;
+                        break;
+                    case "/":
+                        result = arg1 / arg2;
+                        break;
+                    case "*":
+                        result = arg1 * arg2;
+                        break;
+                    default:
+                        break;
+                }
+                values[op.Key] = result;
+                operators.Remove(op.Key);
+            }
+        }
+
+        return values["root"].ToString();
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        var lines = input.Split(Environment.NewLine);
+        var values = new Dictionary<string, Func<long>>();
+        var operators = new Dictionary<string, (string arg1, string arg2, string op)>();
+        foreach (var line in lines)
+        {
+            var match = Regex.Match(line, @"(.*): (.*)");
+            var name = match.Groups[1].Value;
+            var args = match.Groups[2].Value.Split(" ");
+            if (args.Length == 1)
+            {
+                values[name] = () => long.Parse(args[0]);
+            }
+            else
+            {
+                if (name != "root")
+                    operators[name] = (args[0], args[2], args[1]);
+                else
+                    operators[name] = (args[0], args[2], "=");
+            }
+        }
+        var eval = new Dictionary<string, Func<long, long, long>>();
+        while (operators.Count > 0)
+        {
+            var canEvaluate = operators.Where(op => values.ContainsKey(op.Value.arg1) && values.ContainsKey(op.Value.arg2));
+            foreach (var op in canEvaluate)
+            {
+                Func<long> result = null;
+                switch (op.Value.op)
+                {
+                    case "+":
+                        result = () => values[op.Value.arg1]() + values[op.Value.arg2]();
+                        break;
+                    case "-":
+                        result = () => values[op.Value.arg1]() - values[op.Value.arg2]();
+                        break;
+                    case "/":
+                        result = () => values[op.Value.arg1]() / values[op.Value.arg2]();
+                        break;
+                    case "*":
+                        result = () => values[op.Value.arg1]() * values[op.Value.arg2]();
+                        break;
+                    case "=":
+                        result = () =>
+                        {
+                            return values[op.Value.arg1]() - values[op.Value.arg2]();
+                        };
+                        break;
+                    default:
+                        break;
+                }
+                values[op.Key] = result;
+                operators.Remove(op.Key);
+            }
+        }
+        var min = 0L;
+        var max = 10_000_000_000_000L;
+
+        values["humn"] = () => 0;
+        var start = values["root"]();
+
+        values["humn"] = () => max;
+        var end = values["root"]();
+
+        var increasing = end > start;
+
+        var current = 0L;
+        while (true)
+        {
+            current = min + ((max - min) / 2);
+            values["humn"] = () => current;
+            var result = values["root"]();
+            if (result == 0)
+                break;
+            
+            if (increasing)
+            {
+                if (result > 0)
+                {
+                    max = current;
+                }
+                else
+                {
+                    min = current;
+                }
+            }
+            else
+            {
+                if (result > 0)
+                {
+                    min = current;
+                }
+                else
+                {
+                    max = current;
+                }
+            }
+        }
+        while (true)
+        {            
+            values["humn"] = () => current;
+            var result = values["root"]();
+            if (result != 0)
+                break;
+            current--;
+        }
+
+        return (current + 1).ToString();
     }
 }
