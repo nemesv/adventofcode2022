a707ecaa66a3a5b423cdf17a4f38e625e5e32ab0
Viktor Nemes <nemesv@gmail.com>
2022-12-25 09:46:56 +0100

    Solve Day25

--- adventofcode2022.tests/Day25Tests.cs
+++ adventofcode2022.tests/Day25Tests.cs
@@ -3,7 +3,7 @@
 public class Day25Tests : DayTest<Day25>
 {
     [Theory]
-    [InlineData("input", "expected")]
+    [InlineData("1=-0-2\r\n12111\r\n2=0=\r\n21\r\n2=01\r\n111\r\n20012\r\n112\r\n1=-1=\r\n1-12\r\n12\r\n1=\r\n122", "2=-1=0")]
     public void Part1(string input, string output)
     {
         var result = Sut(input.Replace(",", "\r\n")).Part1();
--- adventofcode2022/Day25.cs
+++ adventofcode2022/Day25.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode2022;
+﻿using System.Numerics;
+
+namespace adventofcode2022;
 
 public class Day25 : Day
 {
@@ -8,11 +10,70 @@ public class Day25 : Day
 
     public override string Part1()
     {
-        throw new System.NotImplementedException();
+        var numbers = input.Split(Environment.NewLine);
+        var sum = 0L;
+        foreach (var number in numbers)
+        {
+            sum += ConvertToDecimal(number);
+        }
+        return ConvertToSnafu(sum);
+    }
+
+    private long ConvertToDecimal(string snafu)
+    {
+        var result = 0L;
+        for (int i = 0; i < snafu.Length; i++)
+        {
+            var currrent = snafu[snafu.Length - 1 - i];
+            var mult = (long)Math.Pow(5, i);
+            if (char.IsDigit(currrent))
+            {
+                result += mult * (int)char.GetNumericValue(currrent);
+            }
+            else
+            {
+                if (currrent == '-')
+                {
+                    result -= mult;
+                }
+                else
+                    result -= 2 * mult;
+            }
+        }
+        return result;
+    }
+
+    private string ConvertToSnafu(BigInteger number)
+    {
+        var baseFive = new List<int>();
+        var result = "";
+        while (number > 0)
+        {
+            baseFive.Add((int)(number % 5));
+            number = number / 5;
+        }
+        while (baseFive.Count > 0)
+        {
+            var current = baseFive[0];
+            baseFive.RemoveAt(0);
+            if (current < 3)
+            {
+                result = current.ToString() + result;
+            }
+            else
+            {
+                result = (current == 3 ? "=" : "-") + result;
+                if (baseFive.Count > 0)
+                    baseFive[0] = baseFive[0] + 1;
+                else
+                    baseFive.Add(1);
+            }
+        }
+        return result;
     }
 
     public override string Part2()
     {
-        throw new System.NotImplementedException();
+        return "";
     }
 }
